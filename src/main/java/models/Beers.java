0358aed5ba06670b4e948da2eabcbb24674f48f3
Danyel Murray and Zach Evans <danyel@>
2018-01-09 10:58:18 -0800

    First test: class instantiates correctly

---
+++ src/main/java/models/Beers.java
@@ -0,0 +1,9 @@
+package models;
+
+public class Beers {
+
+
+    public Beers (String content) {
+
+    }
+}
---
+++ src/test/java/models/BeersTest.java
@@ -0,0 +1,25 @@
+package models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class BeersTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+
+    @Test
+    public void instantiatesBeer_true() throws Exception {
+        Beers beers = new Beers("thirsty");
+        assertEquals(true, beers instanceof Beers);
+    }
+}
